2fb8894568394ee720adaf36e1e7d9f4e669f04f
Rinsi <hotabich@mail.ru>
2021-12-28 04:30:48 +0700

    Homework 3 (2021-12-27) - Правки по ревью

--- src/com/company/courseOne/homework3/Task2.java
+++ src/com/company/courseOne/homework3/Task2.java
@@ -28,21 +28,14 @@ public class Task2
 
     public static void main(String[] args)
     {
+        int clientOS = 1;
         int clientDeviceYear = 2005;
-        float clientOS = 1;
+        boolean isLiteVersionNeeded = clientDeviceYear < 2015;
 
         if (clientOS == IOS) {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            }
+            System.out.println("Установите " + (isLiteVersionNeeded ? "облегченную" : "") + " версию приложения для iOS по ссылке");
         } else if (clientOS == ANDROID) {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            }
+            System.out.println("Установите " + (isLiteVersionNeeded ? "облегченную" : "") + " версию приложения для Android по ссылке");
         }
     }
 }
