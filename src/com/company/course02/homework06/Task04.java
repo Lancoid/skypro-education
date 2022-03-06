3ce20f85ad675fa035a4213806e80d78dd6808db
Rinsi <hotabich@mail.ru>
2022-03-07 04:27:27 +0700

    Homework 06 (2022-02-28) - Правки по ревью

--- src/com/company/course02/homework06/Task04.java
+++ src/com/company/course02/homework06/Task04.java
@@ -3,10 +3,14 @@ package com.company.course02.homework06;
 import java.util.*;
 
 /**
- * Напишите код, который выводит в консоль все НЕ уникальные слова из списка слов (в нём могут встречаться дубли).
+ * Напишите код, который выводит в консоль все количество дублей из списка слов.
+ *
+ * *
+ * Например, у вас есть текст из 6 слов, в котором есть одно слово, которое повторяется.
+ * В таком случае в консоли выведется результат "2", то есть то количество элементов, которые повторяются.
+ * *
  *
  * В качестве отладочной информации возьмите произвольный набор слов или текст, в котором встречаются повторения.
- *
  * Код должен работать с любой последовательностью и объемом списка слов.
  */
 public class Task04 {
@@ -14,15 +18,13 @@ public class Task04 {
         List<String> stringList = new ArrayList<>(List.of(
                 "a",
                 "aa",
-                "1aaa",
-                "1aaa",
+                "aaa",
                 "aaaa",
                 "aaaaa",
-                "1aaaaaa",
-                "1aaaaaa",
+                "aaaaaa",
                 "aaaaaaa",
-                "1aaaaaaaa",
-                "1aaaaaaaa"
+                "aaaaaaaa",
+                "aaaaaaaa"
         ));
 
         System.out.println("------------- Выводим всё не уникальные слова.");
@@ -32,8 +34,16 @@ public class Task04 {
     private static void print(List<String> stringList) {
         Set<String> uniques = new HashSet<>(stringList);
 
-        uniques.removeIf(string -> Collections.frequency(stringList, string) == 1);
+        int result = 0;
 
-        System.out.println(uniques);
+        for (String string : uniques) {
+            int frequency = Collections.frequency(stringList, string);
+
+            if (frequency > 1) {
+                result += frequency;
+            }
+        }
+
+        System.out.println(result);
     }
 }
